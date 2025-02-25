2646aaddcd4b75fb186e4d68134ea2f4d3e72a4c
vsmit-coder <vsmit000.01@gmail.com>
2025-02-25 08:53:45 +0530

    Create api.php

---
+++ api.php
@@ -0,0 +1,28 @@
+<?php 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+
+include("cDatabase.php");
+
+$object = new DB;
+$conn = $object->getConnection();
+
+$method = $_SERVER['REQUEST_METHOD'];
+switch($method){
+    case "POST":
+        $user = json_decode(file_get_contents('php://input'));
+        $sql = "INSERT INTO `users` (`name` , `email` , `mobile`) VALUES (:name , :email , :mobile)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bindParam(':name', $user->name);
+        $stmt->bindParam(':email', $user->email);
+        $stmt->bindParam(':mobile', $user->mobile);
+        if($stmt->execute()) {
+            $response = ['status'=> 1 , 'message' => "Success"];
+        }else{
+            $response = ['status'=> 0 , 'message' => "Failed"];
+        }
+        echo json_encode($response);
+        break;
+}
+
+?>
